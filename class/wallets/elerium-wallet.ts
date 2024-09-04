d2ff2c99e9590af556e8159494e6d05ec0ee84af
Maksym Ostapenko <masksym.ostapenko@embrox.com>
2024-09-04 03:07:00 +0300

    feat: generate elerium wallet using nfc card

--- class/wallets/elerium-wallet.ts
+++ class/wallets/elerium-wallet.ts
@@ -1,4 +1,5 @@
-import { HDSegwitBech32Wallet } from './hd-segwit-bech32-wallet.ts';
+import { HDSegwitBech32Wallet } from "./hd-segwit-bech32-wallet.ts";
+import { createWallet, seedWallet } from "@beechatnetwork/elerium-lib";
 
 /**
  * HD Wallet (BIP39).
@@ -6,16 +7,33 @@ import { HDSegwitBech32Wallet } from './hd-segwit-bech32-wallet.ts';
  * @see https://github.com/bitcoin/bips/blob/master/bip-0084.mediawiki
  */
 export class EleriumWallet extends HDSegwitBech32Wallet {
-  static readonly type = 'EleriumWallet';
-  static readonly typeReadable = 'Elerium NFC SegWit (BIP84 Bech32 Native)';
+  static readonly type = "EleriumWallet";
+  static readonly typeReadable = "Elerium NFC SegWit (BIP84 Bech32 Native)";
   // @ts-ignore: override
   public readonly type = EleriumWallet.type;
   // @ts-ignore: override
   public readonly typeReadable = EleriumWallet.typeReadable;
 
+  _seed: number[] = [];
+
   async generate(): Promise<void> {
+    this._seed = await createWallet([]);
     this.secret = "ELERIUM-NFC";
+
+    await this.getAddressAsync()
+    console.log("address", this.getAddress());
+
+    this.signMessage("TEST", this.getAddress());
+  }
+
+  _getSeed(): Buffer {
+    return Buffer.from(this._seed);
+  }
+
+  signMessage(message: string, address: string, useSegwit = true): string {
+    const options = this.segwitType && useSegwit ? { segwitType: this.segwitType } : undefined;
+    const signature = bitcoinMessage.sign(message, "", false, options);
+    return signature.toString('base64');
   }
 
 }
-
